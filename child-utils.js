51e65f4ac978a910459a792594ce7e691533affc
oleks <oleksandrknyga@gmail.com>
2019-05-18 20:42:30 +0300

    init

---
+++ child-utils.js
@@ -0,0 +1,103 @@
+let veevaCLMChildProxy = null;
+class CallbacksRegister {
+  constructor() {
+    this.collection = {};
+    this.pointer = this.getPointerInit();
+  }
+
+  getPointerInit() {
+    return 1;
+  }
+
+  incrementPointer() {
+    this.pointer+=1;
+  }
+
+  register(callback) {
+    const pointer = this.pointer;
+    this.collection[pointer] = callback;
+    this.incrementPointer();
+    return pointer;
+  }
+
+  get(id) {
+    const callback = this.collection[id];
+    this.collection[id] = undefined;
+    return callback;
+  }
+}
+const callbacksRegister = new CallbacksRegister();
+
+const checkIsVeevaCLMEvent = (event) => {
+  if (event.data) {
+    try {
+      const p = JSON.parse(event.data);
+      return p.isVeevaCLM === true && p.result;
+    } catch(e) {
+      return false;
+    }
+  }
+
+  return false;
+};
+
+const handleParentEvent = (event) => {
+  if (checkIsVeevaCLMEvent(event)) {
+    const obj = JSON.parse(event.data);
+    handleVeevaCLMEventResult(obj.result, obj.callbackId, obj);
+  }
+};
+
+const handleVeevaCLMEventResult = (result, callbackId) => {
+  if(!result.success) {
+    return false;
+  }
+
+  callbacksRegister.get(callbackId)(result);
+};
+
+const initvVevaCLMChildProxy = () => {
+  if(veevaCLMChildProxy === null) {
+    if(!window.addEventListener) {
+      window.attachEvent("onmessage", handleParentEvent);
+    } else {
+      window.addEventListener("message", handleParentEvent, false);
+    }
+
+    veevaCLMChildProxy = new Proxy({}, {
+      get(obj, method) {
+        return (...args) => {
+          const event = {
+            isVeevaCLM: true,
+            method,
+            arguments: args.slice(0, args.length-1),
+            callbackId: callbacksRegister.register(args[args.length-1]),
+          };
+          window.parent.postMessage(JSON.stringify(event), '*');
+        };
+      }
+    });
+  }
+
+  return veevaCLMChildProxy;
+};
+
+if (module) {
+  module.exports = {
+    initvVevaCLMChildProxy,
+    handleVeevaCLMEventResult,
+    handleParentEvent,
+    checkIsVeevaCLMEvent,
+    CallbacksRegister,
+    veevaCLMChildProxy,
+  };
+}
+
+if(window) {
+  window.initvVevaCLMChildProxy = initvVevaCLMChildProxy;
+  window.handleVeevaCLMEventResult = handleVeevaCLMEventResult;
+  window.handleParentEvent = handleParentEvent;
+  window.checkIsVeevaCLMEvent = checkIsVeevaCLMEvent;
+  window.CallbacksRegister = CallbacksRegister;
+  window.veevaCLMChildProxy = veevaCLMChildProxy;
+}
---
+++ parent-utils.js
@@ -0,0 +1,69 @@
+const proxyAllIframes = () => {
+  const iframes = document.getElementsByTagName('iframe');
+  for (let i=0; i<iframes.length; i+=1) {
+    proxyIframe(iframes[i]);
+  }
+};
+
+const checkIsVeevaCLMEvent = (event) => {
+  if (event.data) {
+    try {
+      const p = JSON.parse(event.data);
+      return p.isVeevaCLM === true;
+    } catch(e) {
+      return false;
+    }
+  }
+
+  return false;
+};
+
+const proxyIframe = (iframe) => {
+  const handleEvent = (event) => {
+    if(checkIsVeevaCLMEvent(event)) {
+      const {method, callbackId, arguments} = JSON.parse(event.data);
+      com.veeva.clm[method].apply(com.veeva.clm, arguments.concat([(result) => {
+          const data = {
+            isVeevaCLM: true,
+            callbackId: callbackId,
+            result,
+          };
+      iframe.contentWindow.postMessage(JSON.stringify(data), '*');
+    }]));
+    }
+  };
+
+  if(!window.addEventListener) {
+    window.attachEvent("onmessage", handleEvent);
+  } else {
+    window.addEventListener("message", handleEvent, false);
+  }
+};
+
+const proxyAllIframesOnLoad = () => {
+  if (!window || !window.document || !com || !com.veeva || !com.veeva.clm) {
+    return;
+  }
+
+  if(window.addEventListener) {
+    window.addEventListener('load', proxyAllIframes, false);
+  } else {
+    window.onload = proxyAllIframes;
+  }
+};
+
+if (module) {
+  module.exports = {
+    proxyAllIframesOnLoad,
+    proxyAllIframes,
+    proxyIframe,
+    checkIsVeevaCLMEvent,
+  };
+}
+
+if(window) {
+  window.proxyAllIframesOnLoad = proxyAllIframesOnLoad;
+  window.proxyAllIframes = proxyAllIframes;
+  window.proxyIframe = proxyIframe;
+  window.checkIsVeevaCLMEvent = checkIsVeevaCLMEvent;
+}
